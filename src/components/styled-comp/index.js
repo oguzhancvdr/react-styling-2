cadf767cfa5137a863637070f03155564fe5046a
ogzhn <oguzhancavdar06@gmail.com>
2021-05-22 21:48:36 +0300

    add components

--- src/components/styled-comp/index.js
+++ src/components/styled-comp/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Button from './button/Button';
+import HeaderText from './header/HeaderText';
+import LearnFSD from './link/LearnFSD';
+import Wrapper from './wrapper/Wrapper';
 
 const StyledComponents = () => {
     return (
-        <>
+        <Wrapper bgColor="wheat">
+            <HeaderText>Clarusway</HeaderText>
+            <HeaderText headerColor="purple">Styled Components</HeaderText>
+
+            <LearnFSD secondary href="https://clarusway.com/" target="_blank" >Learn Full Stack Development</LearnFSD>
             <Button primary>Primary</Button>
-        </>
+            <Button>Default</Button>
+        </Wrapper>
     )
 }
 
