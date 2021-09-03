17802b6f50fe01d65902f16223da72beed4a544d
ogzhn <oguzhancavdar06@gmail.com>
2021-09-03 23:32:48 +0300

    modify App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
 import "./App.css";
-// import StyledComponents from './components/styled-comp/index'
-import Buttons from "./components/material-ui/buttons/Buttons";
-import CheckBoxComp from "./components/material-ui/checkbox/CheckBoxComp";
-import StyledButton from "./components/material-ui/styled-button/StyledButton";
-import TextFieldComp from "./components/material-ui/text-field/TextFieldComp";
-import Typography from "./components/material-ui/typography/TypographyComp";
-import Wrapper from "./components/material-ui/wrapper/Wrapper";
-import Cards from './components/material-ui/cards/Cards';
-import AppBarComp from './components/material-ui/app-bar/AppBarComp';
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { orange, green } from "@material-ui/core/colors";
-
-const theme = createMuiTheme({
-  typography: {
-    h2: {
-      fontSize: 38,
-      marginBottom: 5,
-    },
-    subtitle1: {
-      marginBottom: 10,
-    },
-  },
-  palette: {
-    primary: {
-      main: orange[400],
-    },
-    secondary: {
-      main: green[400],
-    },
-  },
-});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Wrapper>
-        <AppBarComp />
-        <div className="App">
-          <header className="App-header">
-            <Typography />
-            <StyledButton />
-            <Cards />
-            <TextFieldComp />
-            <CheckBoxComp />
-            <Buttons />
-            {/* <StyledComponents /> */}
-          </header>
-        </div>
-      </Wrapper>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <h1>Styled Components & Material- UI</h1>
+      </header>
+    </div>
   );
 }
 export default App;
