d6d06acaa03fb113f82c372d28c671b6a5a5e2ae
ogzhn <oguzhancavdar06@gmail.com>
2021-09-03 23:43:38 +0300

    modfiy StyledButton comp

--- src/components/material-ui/styled-button/StyledButton.js
+++ src/components/material-ui/styled-button/StyledButton.js
@@ -1,26 +1,11 @@
-import React from 'react'
-import Button from '@material-ui/core/Button/Button'
-import { makeStyles } from "@material-ui/core/styles"
-
-const useStyles = makeStyles({
-    root:{
-        background: "Linear-gradient(45deg, #FF688B, #FF8E53)",
-        border:0,
-        borderRadius:7,
-        color:"#fff",
-        padding: "5px 30px",
-        marginBottom:"1rem",
-
-    }
-})
+import React from "react";
 
 const StyledButton = () => {
-    const classes = useStyles();
-    return (
-        <>
-            <Button className={classes.root}>Styled Button</Button>
-        </>
-    )
-}
+  return (
+    <>
+      <div>Styled Button here</div>
+    </>
+  );
+};
 
-export default StyledButton
+export default StyledButton;
