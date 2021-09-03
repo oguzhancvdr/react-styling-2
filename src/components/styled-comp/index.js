a9b4017c9f055ae79423df4b489deba9073dd575
ogzhn <oguzhancavdar06@gmail.com>
2021-09-03 23:44:39 +0300

    modfiy index.js

--- src/components/styled-comp/index.js
+++ src/components/styled-comp/index.js
@@ -1,25 +1,10 @@
 import React from "react";
-import Button from "./button/Button";
-import ExtendedLearnFSD from "./extended-link/ExtendedLearnFSD";
-import HeaderText from "./header/HeaderText";
-import LearnFSD from "./link/LearnFSD";
-import Wrapper from "./wrapper/Wrapper";
 
 const StyledComponents = () => {
-  const [primary, setPrimary] = React.useState(false);
   return (
-    <Wrapper bgColor="wheat">
-      <HeaderText>Clarusway</HeaderText>
-      <HeaderText headerColor="purple">Styled Components</HeaderText>
-      <LearnFSD secondary href="https://clarusway.com/" target="_blank">
-        Learn Full Stack Development
-      </LearnFSD>
-      <ExtendedLearnFSD>We're Learning Styled Components</ExtendedLearnFSD>
-      <Button primary={primary} onClick={() => setPrimary(!primary)}>
-        {primary ? "Primary": "Default"}
-      </Button>
-      <Button primary={!primary}>{!primary ? "Primary" : "Default" }</Button>
-    </Wrapper>
+    <div>
+      <p>StyledParent Comp here</p>
+    </div>
   );
 };
 
