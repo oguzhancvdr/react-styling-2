d1e2652194e24ffdc58a66b5e66528c733031578
ogzhn <oguzhancavdar06@gmail.com>
2021-09-03 23:32:35 +0300

    rename component files

--- src/components/styled-comp/extended-link/ExtendedLearnFSD.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import LearnFSD from '../link/LearnFSD'
-
-export default styled(LearnFSD)`
-    background-color: green;
-    margin-top:1rem;
-    margin-bottom:1rem;
-`
