6602e11b36ccb1234e022511c7a47e265dc2e8dc
ogzhn <oguzhancavdar06@gmail.com>
2021-09-03 23:35:24 +0300

    modfiy Buttons

--- src/components/material-ui/buttons/Buttons.js
+++ src/components/material-ui/buttons/Buttons.js
@@ -1,19 +1,10 @@
 import React from "react";
-import Button from "@material-ui/core/Button/Button";
-import SaveIcon from "@material-ui/icons/Save";
-import DeleteIcon from '@material-ui/icons/Delete';
-import ButtonGroup from '@material-ui/core/ButtonGroup/ButtonGroup';
 
 const Buttons = () => {
   return (
-    <ButtonGroup  variant="contained" size="large">
-      <Button color="secondary" startIcon={<SaveIcon />}>
-        Save
-      </Button>
-      <Button style={{fontSize:'12px'}} color="primary" endIcon={<DeleteIcon />}>
-        Delete
-      </Button>
-    </ButtonGroup>
+    <div>
+      <p>Buttons here</p>
+    </div>
   );
 };
 
