2c2218386923e5a4eb7386e6b377e1354e1e51e2
ogzhn <oguzhancavdar06@gmail.com>
2021-09-04 17:25:11 +0300

    complete styled comp session

--- src/components/styled-comp/index.js
+++ src/components/styled-comp/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button } from "./button/Button.styles";
+import ExtendedComp from "./extended-link/ExtendedComp";
 import HeaderText from "./header/HeaderText";
+import Link from "./link/Link";
 import Wrapper from "./wrapper/Wrapper";
 
 const StyledComponents = () => {
@@ -8,6 +10,16 @@ const StyledComponents = () => {
     <Wrapper>
       <HeaderText>Clarusway </HeaderText>
       <HeaderText headerColor="red"> Header with props </HeaderText>
+      <Link
+        secondary
+        href="https://clarusway.com/"
+        target="_blank"
+        rel="noopener"
+      >
+        Learn FS development
+      </Link>
+
+      <ExtendedComp> extended from Link comp </ExtendedComp>
       <Button onClick={() => console.log(`clicked`)}>Default</Button>
       <Button primary>Primary</Button>
     </Wrapper>
---
+++ src/components/styled-comp/link/Link.js
@@ -0,0 +1,20 @@
+import styled, { css } from "styled-components";
+
+export default styled.a`
+  font-size: 1.2rem;
+  transition: ease 0.25s all;
+  text-decoration: none;
+  color: #fff;
+  ${(props) =>
+    props.secondary &&
+    css`
+      background-color: #7e3b8e;
+      border-radius: 8px;
+      padding: 8px;
+    `}
+    
+  &:hover {
+    font-size: 1.5rem;
+    opacity: 0.6;
+  }
+`;
